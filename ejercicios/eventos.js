3fc59830e4599f060b61dce143ffcdca31ef77e9
pepesan <pepesan@gmail.com>
2018-01-17 09:17:27 +0100

    enunciados de ejercicios de eventos y filtros

---
+++ ejercicios/eventos.js
@@ -0,0 +1,12 @@
+/*
+1.- Crea un fichero eventos.html e incluye JQuery
+2.- Incluye un botón con el id miboton, haz que cuando se pulse el botón saque un mensaje por pantalla
+3.- Incluye otro botón con el id otroboton, haz que cuando se pulse el botón coloque un borde de color rojo al botón
+3.- Incluye otro botón con el id tercerboton, haz que cuando se pulse el botón coloque un fondo verde al botón
+4.- Incluye un campo de texto con el id campo, haz que cuando adquiera el foco saque un mensaje por consola
+5.- Haz que al perder el foco en el campo de texto, saque un mensaje por pantalla
+6.- Incluye un formulario de login en el HTML, haz que no envíe el formulario si no se han rellenado los dos campos
+7.- Coloca un botón en el HTML y haz que cuando me hagan click en el saque un mensaje por consola
+8.- Coloca un enlace en el HTML a google, haz que cuando entre el ratón saque un mensaje por consola y cuando salga el ratón también
+9.- Haz que cuando se salga del campo del nombre del formulario valide si está rellenado
+*/
---
+++ ejercicios/filtros.js
@@ -0,0 +1,13 @@
+/*
+1. Crea un fichero filtros.html e incluye Jquery
+
+2. Incluye 3 listados desordenado de 10 elementos de texto cada uno
+
+3. Haz que el tercer elemento del primer listado tenga el fondo amarillo
+
+4. Haz que el cuarto elemento del primer listado tenga el fondo amarillo, pero seleccionando primero el tercer elemento
+
+5. Haz que el primer ul tenga un id pero entrando desde el tercer elemento
+
+6. Haz que en el segundo listado todos los elementos tengan un fondo rojo pero entrando desde el primer elemento
+*/
