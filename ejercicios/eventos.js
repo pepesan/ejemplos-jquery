be7ec852bdb3ff3061e56c6783e22cd51d6af568
pepesan <pepesan@gmail.com>
2018-01-17 14:35:54 +0100

    resolución ejercicios de filtros y eventos

--- ejercicios/eventos.js
+++ ejercicios/eventos.js
@@ -1,12 +1,95 @@
 /*
 1.- Crea un fichero eventos.html e incluye JQuery
+*/
+function pulsado(){
+    console.log("Botón pulsado");
+}
+function cambiaBorde(){
+    $("#otroboton").css("border","1px red solid");
+    $(this).css("border","1px red solid");
+}
+function cambiaFondo(){
+    $(this).css("background-color","green");
+    //$("#tercerboton").css("background-color","green");
+}
+function muestraBlur(){
+    console.log("blur");
+}
+function muestraFocus(){
+    console.log("focus");
+}
+function gestionaLogin(evento){
+    
+    if($("#username").val()!="" &&
+      $("#contra").val()!=""){
+       //caso de que el formulario valide
+       
+    }else{
+       //Caso de novalidación
+       evento.preventDefault();
+    }
+}
+function entrado(){
+    console.log("ha entrado");
+}
+function salido(){
+    console.log("ha salido");
+}
+function validaCampo(){
+    if($("#campo").val()!=""){
+        $("#campo").css("border","1px solid green");
+    }else{
+        $("#campo").css("border","1px solid red");
+    }
+}
+function init(){
+    console.log("Dom cargado");
+    /*
 2.- Incluye un botón con el id miboton, haz que cuando se pulse el botón saque un mensaje por pantalla
+*/
+    $("#miboton").click(pulsado);
+    
+/*
+
 3.- Incluye otro botón con el id otroboton, haz que cuando se pulse el botón coloque un borde de color rojo al botón
+*/
+    $("#otroboton").click(cambiaBorde);
+    
+/*
 3.- Incluye otro botón con el id tercerboton, haz que cuando se pulse el botón coloque un fondo verde al botón
+*/
+    $("#tercerboton").click(cambiaFondo);
+    /*
 4.- Incluye un campo de texto con el id campo, haz que cuando adquiera el foco saque un mensaje por consola
 5.- Haz que al perder el foco en el campo de texto, saque un mensaje por pantalla
+*/
+    var campo=$("#campo");
+    campo.focus(muestraFocus);
+    campo.blur(muestraBlur);
+    /*
 6.- Incluye un formulario de login en el HTML, haz que no envíe el formulario si no se han rellenado los dos campos
+*/
+    $("#login").submit(gestionaLogin);
+    /*
 7.- Coloca un botón en el HTML y haz que cuando me hagan click en el saque un mensaje por consola
+*/
+    $("#b1").click(pulsado);
+    /*
 8.- Coloca un enlace en el HTML a google, haz que cuando entre el ratón saque un mensaje por consola y cuando salga el ratón también
+
+*/
+    var enlace=$("#enlace");
+    enlace.mouseover(entrado);
+    enlace.mouseout(salido);
+    /*
 9.- Haz que cuando se salga del campo del nombre del formulario valide si está rellenado
 */
+    campo.blur(validaCampo);
+    
+}
+$(document).ready(init);
+
+
+
+
+
--- ejercicios/filtros.js
+++ ejercicios/filtros.js
@@ -4,10 +4,27 @@
 2. Incluye 3 listados desordenado de 10 elementos de texto cada uno
 
 3. Haz que el tercer elemento del primer listado tenga el fondo amarillo
-
+*/
+function init(){
+    console.log("Dom cargado");
+    //$("ul:first li:nth-child(3)").css("background-color","yellow");
+    $("ul:first li").eq(2).css("background-color","yellow");
+    /*
 4. Haz que el cuarto elemento del primer listado tenga el fondo amarillo, pero seleccionando primero el tercer elemento
+*/
+    $("ul:first li").eq(2).next().css("background-color","yellow");
+    /*
 
 5. Haz que el primer ul tenga un id pero entrando desde el tercer elemento
+*/
+    $("ul:first li").eq(2).parent().attr("id","primero");
+    /*
 
 6. Haz que en el segundo listado todos los elementos tengan un fondo rojo pero entrando desde el primer elemento
 */
+    $("body ul:nth-child(3) li:first").parent().children().css("background-color","red");
+    //$("ul:eq(1) li:first").parent().children().css("background-color","red");
+}
+$(document).ready(init);
+
+
