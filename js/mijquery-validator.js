0fa8803dd06ffd0cc0441bfa585a491fa7d891d0
pepesan <pepesan@gmail.com>
2018-01-17 17:58:29 +0100

    mejora de ejemplo de validacion jquery

--- js/mijquery-validator.js
+++ js/mijquery-validator.js
@@ -2,20 +2,26 @@ function init(){
     $("#login").validate();
     $("#commentForm").validate();
     $("#commentForm2").validate({
-      rules: {
-        name: "required",
-        email: {
-          required: true,
-          email: true
+        rules: {
+            nombre: {
+                "required":true,
+                minlength:4
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            nombre: {
+                required:"Por favor intrduzca un nombre",
+                minlength: "Son necesarios al menos 4 caracteres!"
+            },
+            email: {
+                required: "Necesitamos su email para poder ponernos en contacto con usted",
+                email: "Su email debe tener el formato similar a usuario@dominio.com"
+            }
         }
-      },
-      messages: {
-        name: "Por favor intrduzca un nombre",
-        email: {
-          required: "Necesitamos su email para poder ponernos en contacto con usted",
-          email: "Su email debe tener el formato similar a usuario@dominio.com"
-        }
-      }
     });
     //Revisar la documentación del plugin para más datos https://jqueryvalidation.org/documentation/
 }
