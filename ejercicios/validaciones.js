457b7596320a87de7101d0bbee095c8bec14f58b
pepesan <pepesan@gmail.com>
2018-01-18 17:18:03 +0100

    ejercicios de validaciones y ajax

---
+++ ejercicios/validaciones.js
@@ -0,0 +1,13 @@
+/*
+Validaciones
+
+1. crea un formulario típico de login en el HTML, con dos campos usuario y contraseña
+
+2. Haz que cuando se pulse el botón de enviar el formulario valide si el nombre de usuario y la contraseña están rellenados o no y sólo si es así deje enviar el formulario, en caso contrario deberá informar al usuario de qué campo o campos faltan por rellenar.
+
+3. Haz el típico formulario de registro con los campos: nombre, email, repetir email, contraseña, repetir contraseña y aceptar condiciones del servicio. Con el botón de enviar el formulario
+
+4. Valida el formulario, haciendo que el nombre tenga al menos 2 caracteres y sea requerido, el email sea valido y coincida en ambos campos, la contraseña tenga al menos 8 caracteres y coincidan y haya aceptado las condiciones del servicio.
+
+(validador equalTo())
+*/
