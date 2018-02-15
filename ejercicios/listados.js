e695a5ddb9fbb3a8a43579fa01013fd14c41b642
pepesan <pepesan@gmail.com>
2018-02-15 11:01:54 +0100

    incluido filtros de datos con un keyup

--- ejercicios/listados.js
+++ ejercicios/listados.js
@@ -1,7 +1,9 @@
 var app={
     resultados:[],
+    resultadosFiltrados:[],
     presentaListado:function(){
-        $.each(app.resultados,function(i,item){
+        $("#listado").html("");
+        $.each(app.resultadosFiltrados,function(i,item){
             var nuevoLi="<li><a href='#'>"+item.titulo
             //+app.calculaBotones()
             +"</a></li>";
@@ -15,7 +17,8 @@ var app={
         */
     },
     presentaTabla:function(){
-        $.each(app.resultados,function(i,item){
+        $("#tabla tbody").html("");
+        $.each(app.resultadosFiltrados,function(i,item){
               var nuevaFila="<tr>"
               +"<td>"+item.titulo+"</td>"
               +"<td>"+item.autor+"</td>"
@@ -57,7 +60,8 @@ var app={
     },
     presentaGrid:function(){
         var filaPar=0;
-        for( var item of app.resultados){
+        $(".container").html("");
+        for( var item of app.resultadosFiltrados){
               if(filaPar==0){
                   filaPar=1;
                   //presenta row
@@ -73,15 +77,67 @@ var app={
               }
           }
     },
+    /*
+    6.- Colocar una caja de búsqueda que sirva de filtro para los listados donde se buscará sui el patrón indicado está presente en los dos campos
+    */
+    filtraPorTitulo:function(patron){
+        app.resultadosFiltrados=[];
+        for(var item of app.resultados){
+            if(
+                item.titulo.includes(patron)
+                )
+            ){
+                app.resultadosFiltrados.push(item);
+            }
+        }
+    },
+    filtraPorAutor:function(patron){
+        app.resultadosFiltrados=[];
+        for(var item of app.resultados){
+            if(
+                item.autor!=undefined 
+                    &&
+                item.autor.includes(patron)
+            ){
+                app.resultadosFiltrados.push(item);
+            }
+        }
+    },
+    filtraPorDosCampos:function(patron){
+        app.resultadosFiltrados=[];
+        for(var item of app.resultados){
+            if(
+                item.titulo.includes(patron)
+                || 
+                (   item.autor!=undefined 
+                    &&
+                    item.autor.includes(patron)
+                )
+            ){
+                app.resultadosFiltrados.push(item);
+            }
+        }
+    },
+    filtraDatos:function(){
+        var patron=$(this).val();
+        app.filtraPorDosCampos(patron);
+        console.log(app.resultadosFiltrados);
+        app.presentaListado();
+        app.presentaTabla();
+        app.presentaGrid();
+        
+    },
     peticionAjax:function(){
         $.getJSON(
             "http://www.mocky.io/v2/5a54ae822d00005f235b1cd2",
         function(datos){
             app.resultados=datos;
+            app.resultadosFiltrados=datos;
             console.log(app.resultados);
             app.presentaListado();
             app.presentaTabla();
             app.presentaGrid();
+            $("#buscar").keyup(app.filtraDatos);
         })
         .fail(function(){
             console.log("error");
@@ -105,4 +161,5 @@ $(document).ready(app.init);
 3.- Presentar los datos en un formato listado (ul+li)
 4.- Presentar los datos en un formato de tabla (tr+td)
 5.- Presentar los datos en un formato grid (bootstrap+col-6)
+6.- Colocar una caja de búsqueda que sirva de filtro para los listados
 */
